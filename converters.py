4aea8ef8bb5b4e49d8be9c38566321d15dfe16e3
anwar8105 <anwarrnadaf@gmail.com>
2021-10-10 13:23:15 +0530

    implemented class

--- converters.py
+++ converters.py
@@ -1,18 +1,20 @@
 from morse_code import morseCode
 
 
-def convertToMorse(lst, words):
-    for letters in words:
-        if letters in morseCode.keys():
-            lst.append((letters, morseCode[letters]))   # append as tuple
+class Converter:
+    def convert_to_morse(lst, words):
+        """Accepts strings and returns morse_code"""
+        for letters in words:
+            if letters in morseCode.keys():
+                lst.append((letters, morseCode[letters]))   # append as tuple
 
-    return lst
+        return lst
 
+    def convert_to_alpha(converted_code, lst):
+        """Accepts morse_code and returns strings"""
+        for codes in lst:
+            for key, value in morseCode.items():
+                if codes == value:
+                    converted_code.append((value, key))
 
-def convertToAlpha(converted_code, lst):
-    for codes in lst:
-        for key, value in morseCode.items():
-            if codes == value:
-                converted_code.append((value, key))
-
-    return converted_code
+        return converted_code
